ea4ea248a93aea14761890d0d5dda27afa52e135
KunalKohinkar <kohinkarkunal23@gmail.com>
2022-10-21 01:32:07 +0530

    feedback not working

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Database, set, ref, update } from '@angular/fire/database';
+import { Database, set, ref, update, get } from '@angular/fire/database';
 import { AppComponent} from '../app.component'
+import { getDatabase, onValue } from "firebase/database";
+import { firebaseApp$ } from '@angular/fire/app';
 
 @Component({
   selector: 'app-feedback',
@@ -11,27 +13,30 @@ export class FeedbackComponent implements OnInit {
   feedbacks = [
     {
     name:"Paresh Pandit",
-    feedback:"Good Day"
+    feedback:"Good Day",
+    ID:1
     }
     ]
     
   constructor(public database:Database) {
    }
-  sub(a:any,b:any){
+  sub(a:any,b:any,c:any){
    let tem_struct = {
-      name:a,
-      feedback:b
+      name:b,
+      feedback:c,
+      ID:a
       }
     
     this.feedbacks.push(tem_struct);
     
-    set(ref(this.database, 'feedbacks/' + tem_struct.name ), {
+    set(ref(this.database, 'feedbacks/' + tem_struct.ID ), {
       Name:tem_struct.name,
       Feedback:tem_struct.feedback
     });
     alert("Feedback registered successfully!!!");
-    
 
+  
+   
   }
   ngOnInit(): void {
   }
